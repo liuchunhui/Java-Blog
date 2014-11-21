ac92cf32fedd5abb519539d26b31ad9ca022b5f0
liuchunhui <chunhui_liu@163.com>
2014-11-21 15:47:19 +0800

    增加登录功能,Struts加入成功运行

--- BookManageSystem/src/main/java/com/huihui/Test.java
+++ BookManageSystem/src/main/java/com/huihui/Test.java
@@ -13,28 +13,28 @@ import org.hibernate.service.ServiceRegistryBuilder;
 public class Test {
 
     public static void main(String[] args) {
-        // --------------实例化Configration----------------------
-        Configuration conf = new Configuration().configure();
-
-        ServiceRegistry serviceRegistry = new ServiceRegistryBuilder()
-                .applySettings(conf.getProperties()).buildServiceRegistry();
-        //-------------------END----------------------------------
-
-        // 以Configuration实例创建SessionFactory实例
-        SessionFactory sf = conf.buildSessionFactory(serviceRegistry);
-        // 创建Session
-        Session sess = sf.openSession();
-        // 开始事务
-        Transaction tx = sess.beginTransaction();
-
-        //  提交事务
-        tx.commit();
-        // 关闭Session
-        sess.close();
-
-        sf.close();
-
-
+//        // --------------实例化Configration----------------------
+//        Configuration conf = new Configuration().configure();
+//
+//        ServiceRegistry serviceRegistry = new ServiceRegistryBuilder()
+//                .applySettings(conf.getProperties()).buildServiceRegistry();
+//        //-------------------END----------------------------------
+//
+//        // 以Configuration实例创建SessionFactory实例
+//        SessionFactory sf = conf.buildSessionFactory(serviceRegistry);
+//        // 创建Session
+//        Session sess = sf.openSession();
+//        // 开始事务
+//        Transaction tx = sess.beginTransaction();
+//
+//        //  提交事务
+//        tx.commit();
+//        // 关闭Session
+//        sess.close();
+//
+//        sf.close();
+//
+//
     }
 
 
---
+++ BookManageSystem/src/web/WEB-INF/content/error.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: huihui
+  Date: 14-11-21
+  Time: 上午10:31
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="s" uri="/struts-tags" %>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+    <title><s:text name="errorPage" /></title>
+</head>
+<body>
+    <s:text name="errorPage" />
+</body>
+</html>
